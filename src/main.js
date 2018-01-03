b7df4ba6c12a61e13af132635f5a289492d175a8
Raine Virta <raine.virta@gmail.com>
2018-01-03 14:51:34 +0200

    main: add POST /update to handle github webhook

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import serveIndex from 'serve-index'
+import bodyParser from 'body-parser'
 import * as git from './git'
+import stringify from 'json-stringify-pretty-compact'
 import log from './logger'
 import R from 'ramda'
 
@@ -20,6 +22,7 @@ const [
 ])
 
 const app = express()
+app.use(bodyParser.json())
 app.listen(port, () =>
   log.info(`app listening on port ${port}!`))
 
@@ -43,6 +46,12 @@ const updateRepoWithHardReset = async (repo) => {
 ;(async () => {
   const repo = await cloneOrOpenRepository(repoUrl, repoPath)
   await updateRepoWithHardReset(repo)
+
+  app.post('/update', (req, res) => {
+    log.info(`update webhook endpoint triggered ${stringify(req.body)}`)
+    updateRepoWithHardReset(repo)
+    res.status(200).end()
+  })
 
   app.use('/',
     express.static(repoPath),
