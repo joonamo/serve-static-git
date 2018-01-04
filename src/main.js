428fd873eec3e22176ca549235877aad209b96b9
Raine Virta <raine.virta@gmail.com>
2018-01-04 14:36:01 +0200

    main: reorganize

--- src/main.js
+++ src/main.js
@@ -9,9 +9,43 @@ import log from './logger'
 import config from './config'
 import unless from './unless'
 
-const app = express()
-app.use(bodyParser.json())
+const setupRepo = async (path, url, creds) => {
+  const repo = await git.openOrClone(url, path, creds)
+  await git.fetchAndHardReset(repo, creds)
+  return repo
+}
 
+const setup = async (app, config) => {
+  const path = config.REPO_PATH
+  const url = config.REPO_URL
+  const creds = {
+    pubKey: config.SSH_PUBLIC_KEY,
+    privKey: config.SSH_PRIVATE_KEY
+  }
+
+  const repo = await setupRepo(path, url, creds)
+
+  app.post('/update', async (req, res) => {
+    log.info(`update webhook endpoint triggered ${stringify(req.body)}`)
+    await git.fetchAndHardReset(repo, creds)
+    res.status(200).end()
+  })
+
+  app.use('/',
+    express.static(path),
+    serveIndex(path, {
+      icons: true
+    }))
+
+  log.info(`serving ${path} at /`)
+
+  app.listen(config.PORT, () =>
+    log.info(`app listening on port ${config.PORT}!`))
+}
+
+const app = express()
+
+app.use(bodyParser.json())
 app.use(unless('POST', '/update', session({
   secret: config.SESSION_SECRET,
   resave: false,
@@ -25,37 +59,10 @@ app.use(unless('POST', '/update', gauth({
   allowedDomains: config.GOOGLE_OAUTH_ALLOWED_DOMAINS.split(',')
 })))
 
-const setup = async () => {
-  const repoPath = config.REPO_PATH
-  const repoUrl = config.REPO_URL
-  const creds = {
-    pubKey: config.SSH_PUBLIC_KEY,
-    privKey: config.SSH_PRIVATE_KEY
-  }
-  const repo = await git.openOrClone(repoUrl, repoPath, creds)
-  await git.fetchAndHardReset(repo, creds)
-
-  app.post('/update', async (req, res) => {
-    log.info(`update webhook endpoint triggered ${stringify(req.body)}`)
-    await git.fetchAndHardReset(repo, creds)
-    res.status(200).end()
-  })
-
-  app.use('/',
-    express.static(repoPath),
-    serveIndex(repoPath, {
-      icons: true
-    }))
-
-  log.info(`serving ${repoPath} at /`)
-
-  app.listen(config.PORT, () =>
-    log.info(`app listening on port ${config.PORT}!`))
-}
-
-setup()
+setup(app, config)
   .catch((err) => {
     log.error(err.stack)
+    process.exit(1)
   })
 
 process.on('unhandledRejection', (err) => {
